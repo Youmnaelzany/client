09e22c0b6b879479484ad3cd3f92f8eceda7d866
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2025-06-12 20:21:41 +0300

    feat: implement routing and structure for main application

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movieDetails" element={<MovieDetails />} />
+        <Route path="/favorite" element={<Favorite />} />
+        <Route path="/myBookings" element={<MyBookings />} />
+        <Route path="/seatLayout" element={<SeatLayout />} />
+      </Routes>
+    </>
+  );
 };
 export default App;
